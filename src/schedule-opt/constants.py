231458aa84a950722998e3b8875dd65b6ef0b46f
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2024-12-28 22:43:22 +0100

    feat: added localsearch constants

--- src/schedule-opt/constants.py
+++ src/schedule-opt/constants.py
@@ -3,6 +3,11 @@ START_TIME = 8  # Start of the day
 END_TIME = 18  # End of the day
 CLASSROOMS: list[str] = ["N18", "N11", "N13"]
 
+MAX_ITER = 1000
+MAX_MOVES = 1 # max number of consecutive moves to make for the same slot
+ALPHA = 1 # weight for the empty slots
+BETA = 1 # weight for the teacher slot preference
+
 COLORS: list[str] = [
     "#556B2F", # Dark Olive Green
     "#8B4513", # Saddle Brown
